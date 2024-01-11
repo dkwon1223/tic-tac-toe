ea8a77c0549f5a6e721c160aa9688f45f008eb84
David Kwon <dkwon1223@gmail.com>
2024-01-11 01:38:41 -0700

    Implement function playTurn in main.js

--- main.js
+++ main.js
@@ -43,3 +43,25 @@ function displayGameStats() {
         currentTurn: null
     };
 }
+
+// A function for playing a turn
+function playTurn(index, playerNum) {
+    if(gameBoard[index] === 0) {
+        turnCounter++;
+        gameBoard[index] = playerNum;
+        console.log(gameBoard.slice(0,3));
+        console.log(gameBoard.slice(3,6));
+        console.log(gameBoard.slice(6));
+        trackTurn();
+        checkWin();
+        checkDraw();
+        console.log("\n");
+    } else {
+        console.log(gameBoard.slice(0,3));
+        console.log(gameBoard.slice(3,6));
+        console.log(gameBoard.slice(6));
+        console.log("\nCannot play here.");
+        trackTurn();
+        console.log("\n");
+    }
+}
